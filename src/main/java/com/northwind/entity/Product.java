c90172a53f4f7650e9cf493eec830581415f6278
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-20 20:49:38 -0500

    Finalizado el desarrolllo de la clase Products en el paquete entity

--- src/main/java/com/northwind/entity/Product.java
+++ src/main/java/com/northwind/entity/Product.java
@@ -1,8 +1,25 @@
 package com.northwind.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import java.math.BigDecimal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "Products")
+@Data
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int productId;
+	private String productName;
+	private int supplierId;
+	private int categoryId;
+	private String unit;
+	private BigDecimal price;
 }
