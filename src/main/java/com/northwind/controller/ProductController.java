73894afc0810a995528f0842ed333298798fbd83
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-21 18:19:09 -0500

    Agregar endpoint PUT '/products/{productId}' para actualizar productos en ProductController

--- src/main/java/com/northwind/controller/ProductController.java
+++ src/main/java/com/northwind/controller/ProductController.java
@@ -1,12 +1,14 @@
 package com.northwind.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +39,25 @@ public class ProductController {
 	public ResponseEntity<Integer> register(@RequestBody Product product) {
 	    Product savedProduct = productRepository.save(product);
 	    return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct.getProductId());
+	}
+	
+	@PutMapping("{productId}")
+	public ResponseEntity<Product> update(@PathVariable int productId, @RequestBody Product updatedProduct){
+		Optional<Product> productOptional = productRepository.findById(productId);
+		if(productOptional.isPresent()) {
+			Product existingProduct  = productOptional.get();
+			existingProduct.setProductName(updatedProduct.getProductName());
+			existingProduct.setSupplierId(updatedProduct.getSupplierId());
+	        existingProduct.setCategoryId(updatedProduct.getCategoryId());
+	        existingProduct.setUnit(updatedProduct.getUnit());
+	        existingProduct.setPrice(updatedProduct.getPrice());
+			
+	        Product updateProductEntity = productRepository.save(existingProduct);
+	        
+	        return ResponseEntity.ok(updateProductEntity);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 }
