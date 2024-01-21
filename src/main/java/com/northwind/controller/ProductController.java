60dca0caa5cb3d4bc3176d5918cc3c58410f5ecc
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-21 14:32:15 -0500

    Actualizar ProductController con anotaciones y dependencia de ProductRepository

--- src/main/java/com/northwind/controller/ProductController.java
+++ src/main/java/com/northwind/controller/ProductController.java
@@ -1,5 +1,16 @@
 package com.northwind.controller;
 
-public class ProductController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.northwind.repository.ProductRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "products")
+public class ProductController {
+	
+	ProductRepository productRepository;
 }
