b9f44b388f0ddb6fd8c768957228f02645be7365
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-21 16:24:09 -0500

    Agregar endpoint GET '/products' en ProductController para obtener todos los productos

--- src/main/java/com/northwind/controller/ProductController.java
+++ src/main/java/com/northwind/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.northwind.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.northwind.entity.Product;
 import com.northwind.repository.ProductRepository;
 
 import lombok.AllArgsConstructor;
@@ -13,4 +17,9 @@ import lombok.AllArgsConstructor;
 public class ProductController {
 	
 	ProductRepository productRepository;
+	
+	@GetMapping
+	public List<Product> findAll(){
+		return productRepository.findAll();
+	}
 }
--- src/main/java/com/northwind/entity/Product.java
+++ src/main/java/com/northwind/entity/Product.java
@@ -2,6 +2,7 @@ package com.northwind.entity;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class Product {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ProductID")
 	private int productId;
 	private String productName;
 	private int supplierId;
