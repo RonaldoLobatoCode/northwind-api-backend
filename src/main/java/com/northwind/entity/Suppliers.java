983f3ea8eb83be55d12d33708a11886731c172b7
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 12:02:45 -0500

    Finalizado el desarrollo de la clase Supplier en el paquete entity

--- src/main/java/com/northwind/entity/Suppliers.java
+++ src/main/java/com/northwind/entity/Suppliers.java
@@ -1,5 +1,39 @@
 package com.northwind.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Suppliers")
 public class Suppliers {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "supplierid")
+	private int supplierId;
+	
+	@Column(name = "suppliername")
+	private String supplierName;
+	
+	@Column(name = "contactname")
+	private String contactName;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "city")
+	private String city;
+	
+	@Column(name = "postalcode")
+	private String postalCode;
+	
+	@Column(name = "country")
+	private String country;
+	
+	@Column(name = "phone")
+	private String phone;
 }
