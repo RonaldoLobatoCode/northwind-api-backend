bd1f81eb33e2b7e8fe575763856ceb8eaf5dc290
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 18:51:17 -0500

    Agregar endpoint GET '/categories/{categoryId}' en CategorieController para buscar una categoria por ID

--- src/main/java/com/northwind/controller/CategorieController.java
+++ src/main/java/com/northwind/controller/CategorieController.java
@@ -2,7 +2,9 @@ package com.northwind.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class CategorieController {
 	@GetMapping
 	public List<Categorie> findAll(){
 		return categorieRepository.findAll();
+	}
+	
+	@GetMapping("{categoryId}")
+	public ResponseEntity<Categorie> findById(@PathVariable int categoryId){
+		return ResponseEntity.of(categorieRepository.findById(categoryId));
 	}
 }
