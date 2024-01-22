31f1a263269679e1ec2d95a6518b5a27ff9cdd59
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 18:46:36 -0500

    Agregar endpoint GET '/categories' en CategorieController para obtener todas las categorias

--- src/main/java/com/northwind/controller/CategorieController.java
+++ src/main/java/com/northwind/controller/CategorieController.java
@@ -1,8 +1,12 @@
 package com.northwind.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.northwind.entity.Categorie;
 import com.northwind.repository.CategorieRepository;
 
 import lombok.AllArgsConstructor;
@@ -13,4 +17,9 @@ import lombok.AllArgsConstructor;
 public class CategorieController {
 
 	CategorieRepository categorieRepository;
+	
+	@GetMapping
+	public List<Categorie> findAll(){
+		return categorieRepository.findAll();
+	}
 }
