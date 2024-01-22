9a9f959cb2b56fdb9f0739e8e098e015cf6519fd
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 09:40:21 -0500

    Agregar endpoint DELETE '/customers/{customerId}' para eliminar un cliente por ID en CustomerController

--- src/main/java/com/northwind/controller/CustomerController.java
+++ src/main/java/com/northwind/controller/CustomerController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +61,11 @@ public class CustomerController {
 		}else {
 			return ResponseEntity.notFound().build();
 		}
+	}
+	
+	@DeleteMapping("{customerId}")
+	public void delete(@PathVariable int customerId) {
+		customerRepository.deleteById(customerId);
 	}
 }
 
