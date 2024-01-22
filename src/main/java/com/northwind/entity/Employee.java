6b36f6055b43fc5c71b9da97cef41074da192fb3
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 10:05:59 -0500

    Finalizado el desarrollo de la clase Employee en el paquete entity

--- src/main/java/com/northwind/entity/Employee.java
+++ src/main/java/com/northwind/entity/Employee.java
@@ -1,5 +1,37 @@
 package com.northwind.entity;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "Employees")
+@Data
 public class Employee {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "employeeid")
+	private int employeeId;
+	
+	@Column(name = "lastname")
+	private String lastName;
+	
+	@Column(name = "firstname")
+	private String firstName;
+	
+	@Column(name = "birthdate")
+	private Date birthDate;
+	
+	@Column(name = "photo")
+	private String photo;
+	
+	@Column(name = "notes")
+	private String notes;
 }
