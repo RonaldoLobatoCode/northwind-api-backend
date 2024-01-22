87f0ed96de98a9f44e4bb93a29d168b49315121e
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 12:26:25 -0500

    Agregar endpoint GET '/suppliers/{supplierId}' en SupplierController para buscar un proveedor por ID

--- src/main/java/com/northwind/controller/SupplierController.java
+++ src/main/java/com/northwind/controller/SupplierController.java
@@ -2,7 +2,9 @@ package com.northwind.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class SupplierController {
 	@GetMapping
 	public List<Supplier> findAll(){
 		return supplierRepository.findAll();
+	}
+	
+	@GetMapping("{supplierId}")
+	public ResponseEntity<Supplier> findById(@PathVariable int supplierId){
+		return ResponseEntity.of(supplierRepository.findById(supplierId));
 	}
 }
