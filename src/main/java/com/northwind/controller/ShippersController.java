66efd50ae6928727564e099b51e12bd88c5e3369
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 18:02:05 -0500

    Agregar endpoint GET '/shippers/{shipperId}' en ShippersController para buscar un transportista por ID

--- src/main/java/com/northwind/controller/ShippersController.java
+++ src/main/java/com/northwind/controller/ShippersController.java
@@ -2,7 +2,9 @@ package com.northwind.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class ShippersController {
 	@GetMapping
 	public List<Shippers> findAll(){
 		return shippersRepository.findAll();
+	}
+	
+	@GetMapping("{shipperId}")
+	public ResponseEntity<Shippers> findById(@PathVariable int shipperId){
+		return ResponseEntity.of(shippersRepository.findById(shipperId));
 	}
 }
