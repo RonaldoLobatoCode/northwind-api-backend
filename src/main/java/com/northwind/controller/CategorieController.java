d2084b0411532edecd950906fd7da0af96172f64
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 18:43:32 -0500

    Actualizar CategorieController con anotaciones y dependencia de CategorieRepository

--- src/main/java/com/northwind/controller/CategorieController.java
+++ src/main/java/com/northwind/controller/CategorieController.java
@@ -1,5 +1,16 @@
 package com.northwind.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.northwind.repository.CategorieRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "categories")
 public class CategorieController {
 
+	CategorieRepository categorieRepository;
 }
