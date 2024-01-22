4e581bb87ec1d5c3b5f21b1c3bf3f3864d8e9417
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 09:22:00 -0500

    Agregar endpoint POST '/customers' para registrar clientes en CustomerController

--- src/main/java/com/northwind/controller/CustomerController.java
+++ src/main/java/com/northwind/controller/CustomerController.java
@@ -2,9 +2,13 @@ package com.northwind.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +33,21 @@ public class CustomerController {
 	public ResponseEntity<Customer> findById(@PathVariable int customerId){
 		return ResponseEntity.of(customerRepository.findById(customerId));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Integer> save(@RequestBody Customer customer){
+		Customer save = customerRepository.save(customer);
+		return ResponseEntity.status(HttpStatus.CREATED).body(save.getCustomerId());
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
