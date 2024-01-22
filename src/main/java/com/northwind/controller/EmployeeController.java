dea1211f88afc9ec9962b470bfa4e0f1fa98b90f
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 10:45:44 -0500

    Agregar endpoint GET '/employees/{employeeId}' en EmployeeController para buscar un empleado por ID

--- src/main/java/com/northwind/controller/EmployeeController.java
+++ src/main/java/com/northwind/controller/EmployeeController.java
@@ -2,7 +2,9 @@ package com.northwind.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class EmployeeController {
 	@GetMapping
 	public List<Employee> findAll(){
 		return employeeRepository.findAll();
+	}
+	
+	@GetMapping("{employeeId}")
+	public ResponseEntity<Employee> findById(@PathVariable int employeeId){
+		return ResponseEntity.of(employeeRepository.findById(employeeId));
 	}
 }
