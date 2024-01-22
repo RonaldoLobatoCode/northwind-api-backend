d2e855fb8e7ed0b43a0c596b99b8c1b240c74855
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 09:11:52 -0500

    Agregar endpoint GET '/customers' en CustomerController para obtener todos los productos

--- src/main/java/com/northwind/controller/CustomerController.java
+++ src/main/java/com/northwind/controller/CustomerController.java
@@ -1,8 +1,14 @@
 package com.northwind.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.northwind.entity.Customer;
 import com.northwind.repository.CustomerRepository;
 
 import lombok.AllArgsConstructor;
@@ -13,4 +19,10 @@ import lombok.AllArgsConstructor;
 public class CustomerController {
 
 	CustomerRepository customerRepository;
+	
+	@GetMapping
+	public List<Customer> findAll(){
+		return customerRepository.findAll();
+	}
+	
 }
