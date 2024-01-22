fb848553466e7a9deae9d990b686348747755a46
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 12:33:38 -0500

    Agregar endpoint POST '/suppliers' para registrar proveedores en SupplierController

--- src/main/java/com/northwind/controller/SupplierController.java
+++ src/main/java/com/northwind/controller/SupplierController.java
@@ -2,9 +2,12 @@ package com.northwind.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +32,14 @@ public class SupplierController {
 	public ResponseEntity<Supplier> findById(@PathVariable int supplierId){
 		return ResponseEntity.of(supplierRepository.findById(supplierId));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Integer> register(@RequestBody Supplier supplier){
+		Supplier save = supplierRepository.save(supplier);
+		return ResponseEntity.status(HttpStatus.CREATED).body(save.getSupplierId());
+	}
 }
+
+
+
+
