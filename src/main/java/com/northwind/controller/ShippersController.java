5cbf81843349e5c4af8ed866f11c5b70edf4c74a
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 18:13:23 -0500

    Agregar endpoint POST '/shippers' para registrar transportistas en ShippersController

--- src/main/java/com/northwind/controller/ShippersController.java
+++ src/main/java/com/northwind/controller/ShippersController.java
@@ -2,9 +2,12 @@ package com.northwind.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +32,19 @@ public class ShippersController {
 	public ResponseEntity<Shippers> findById(@PathVariable int shipperId){
 		return ResponseEntity.of(shippersRepository.findById(shipperId));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Integer> register(@RequestBody Shippers shippers){
+		Shippers save = shippersRepository.save(shippers);
+		return ResponseEntity.status(HttpStatus.CREATED).body(save.getShipperId());
+	}
 }
+
+
+
+
+
+
+
+
+
