16f77c40b29b2d9993daed79129ae458cf845eca
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 12:14:42 -0500

    Actualizar SupplierController con anotaciones y dependencia de SupplierRepository

--- src/main/java/com/northwind/controller/SupplierController.java
+++ src/main/java/com/northwind/controller/SupplierController.java
@@ -1,5 +1,16 @@
 package com.northwind.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.northwind.repository.SupplierRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "suppliers")
 public class SupplierController {
 
+	SupplierRepository supplierRepository;
 }
