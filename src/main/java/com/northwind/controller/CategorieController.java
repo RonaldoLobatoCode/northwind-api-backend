f7c95642c6e5dffe3c51476892eb6b2e71a9eecd
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 18:57:43 -0500

    Agregar endpoint POST '/categories' para registrar categorias en CategorieController

--- src/main/java/com/northwind/controller/CategorieController.java
+++ src/main/java/com/northwind/controller/CategorieController.java
@@ -2,9 +2,12 @@ package com.northwind.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +32,23 @@ public class CategorieController {
 	public ResponseEntity<Categorie> findById(@PathVariable int categoryId){
 		return ResponseEntity.of(categorieRepository.findById(categoryId));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Integer> register(@RequestBody Categorie categorie){
+		Categorie save = categorieRepository.save(categorie);
+		return ResponseEntity.status(HttpStatus.CREATED).body(save.getCategoryId());
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
