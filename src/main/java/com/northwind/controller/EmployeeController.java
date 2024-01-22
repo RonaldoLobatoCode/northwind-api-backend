9578dd3849b174fd5a804524e53512e90ba144d1
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 11:22:38 -0500

    Agregar endpoint POST '/employees' parar registrar empleados en EmployeeController

--- src/main/java/com/northwind/controller/EmployeeController.java
+++ src/main/java/com/northwind/controller/EmployeeController.java
@@ -2,9 +2,12 @@ package com.northwind.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +32,17 @@ public class EmployeeController {
 	public ResponseEntity<Employee> findById(@PathVariable int employeeId){
 		return ResponseEntity.of(employeeRepository.findById(employeeId));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Integer> save(@RequestBody Employee employee){
+		Employee save = employeeRepository.save(employee);
+		return ResponseEntity.status(HttpStatus.CREATED).body(save.getEmployeeId());
+	}
 }
+
+
+
+
+
+
+
