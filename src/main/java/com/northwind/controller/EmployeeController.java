e702fdbf63b1547976f8edaec8906720e548f8cf
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 11:31:40 -0500

    Agregar endpoint PUT 'employees/{employeeId}' para actualizar un empleado por ID en EmployeeController

--- src/main/java/com/northwind/controller/EmployeeController.java
+++ src/main/java/com/northwind/controller/EmployeeController.java
@@ -1,12 +1,14 @@
 package com.northwind.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +39,25 @@ public class EmployeeController {
 	public ResponseEntity<Integer> save(@RequestBody Employee employee){
 		Employee save = employeeRepository.save(employee);
 		return ResponseEntity.status(HttpStatus.CREATED).body(save.getEmployeeId());
+	}
+		
+	@PutMapping("{employeeId}")
+	public ResponseEntity<Employee> update(@PathVariable int employeeId, @RequestBody Employee updatedEmployee){
+		Optional<Employee> employeeOptional = employeeRepository.findById(employeeId);
+		if(employeeOptional.isPresent()) {
+			Employee existingEmployee = employeeOptional.get();
+			existingEmployee.setLastName(updatedEmployee.getLastName());
+			existingEmployee.setFirstName(updatedEmployee.getFirstName());
+			existingEmployee.setBirthDate(updatedEmployee.getBirthDate());
+			existingEmployee.setPhoto(updatedEmployee.getPhoto());
+			existingEmployee.setNotes(updatedEmployee.getNotes());
+			
+			Employee updateEmployeeEntity = employeeRepository.save(existingEmployee);
+			
+			return ResponseEntity.ok(updateEmployeeEntity);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
 
