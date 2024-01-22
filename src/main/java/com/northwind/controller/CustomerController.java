8a344a8e00092ac45211e4c6bc95cb44ef89c70a
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 09:33:29 -0500

    Agregar endpoint PUT '/customers/{customerId}' para actualizar un cliente por ID en CustomerController

--- src/main/java/com/northwind/controller/CustomerController.java
+++ src/main/java/com/northwind/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.northwind.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +40,26 @@ public class CustomerController {
 	public ResponseEntity<Integer> save(@RequestBody Customer customer){
 		Customer save = customerRepository.save(customer);
 		return ResponseEntity.status(HttpStatus.CREATED).body(save.getCustomerId());
+	}
+	
+	@PutMapping("{customerId}")
+	public ResponseEntity<Customer> update(@PathVariable int customerId,@RequestBody Customer updatedCustomer){
+		Optional<Customer> customerOptional = customerRepository.findById(customerId);
+		if(customerOptional.isPresent()) {
+			Customer existingCustomer = customerOptional.get();
+			existingCustomer.setCustomerName(updatedCustomer.getCustomerName());
+			existingCustomer.setContactName(updatedCustomer.getContactName());
+			existingCustomer.setAddress(updatedCustomer.getAddress());
+			existingCustomer.setCity(updatedCustomer.getCity());
+			existingCustomer.setPostalCode(updatedCustomer.getPostalCode());
+			existingCustomer.setCountry(updatedCustomer.getCountry());
+			
+			Customer updateCustomerEntity = customerRepository.save(existingCustomer);
+			
+			return ResponseEntity.ok(updateCustomerEntity);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
 
