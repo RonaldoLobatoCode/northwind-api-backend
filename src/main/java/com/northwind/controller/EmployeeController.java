1f8ea9f5f6a9199f32931b6eb9853bbb03282a4a
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 10:21:19 -0500

    Actualizar EmployeeController con anotaciones y dependencia de EmployeeRepository

--- src/main/java/com/northwind/controller/EmployeeController.java
+++ src/main/java/com/northwind/controller/EmployeeController.java
@@ -1,5 +1,16 @@
 package com.northwind.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.northwind.repository.EmployeeRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "employees")
 public class EmployeeController {
 
+	EmployeeRepository employeeRepository;
 }
