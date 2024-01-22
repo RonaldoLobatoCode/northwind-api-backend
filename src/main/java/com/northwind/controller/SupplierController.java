bd82c458a48691efd7ee94b2152a344639574078
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 12:45:13 -0500

    Agregar endpoint PUT '/suppliers/{supplierId}' para actualizar un proveedor por ID en SupplierController

--- src/main/java/com/northwind/controller/SupplierController.java
+++ src/main/java/com/northwind/controller/SupplierController.java
@@ -1,12 +1,14 @@
 package com.northwind.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +40,39 @@ public class SupplierController {
 		Supplier save = supplierRepository.save(supplier);
 		return ResponseEntity.status(HttpStatus.CREATED).body(save.getSupplierId());
 	}
+	
+	@PutMapping("{supplierId}")
+	public ResponseEntity<Supplier> update(@PathVariable int supplierId, @RequestBody Supplier updatedsupplier){
+		Optional<Supplier> supplierOptional = supplierRepository.findById(supplierId);
+		if(supplierOptional.isPresent()) {
+			Supplier existingSupplier = supplierOptional.get();
+			existingSupplier.setSupplierName(updatedsupplier.getSupplierName());
+			existingSupplier.setContactName(updatedsupplier.getContactName());
+			existingSupplier.setAddress(updatedsupplier.getAddress());
+			existingSupplier.setCity(updatedsupplier.getCity());
+			existingSupplier.setPostalCode(updatedsupplier.getPostalCode());
+			existingSupplier.setCountry(updatedsupplier.getCountry());
+			existingSupplier.setPhone(updatedsupplier.getPhone());
+			
+			Supplier updatedSupplierEntity = supplierRepository.save(existingSupplier);
+			return ResponseEntity.ok(updatedSupplierEntity);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
