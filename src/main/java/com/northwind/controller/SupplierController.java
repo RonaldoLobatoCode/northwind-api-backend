408f3e523ee7263c5e72957c7e28062625f3800e
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 12:48:00 -0500

    Agregar endpoint DELETE '/suppliers/{supplierId}' para eliminar un proveedor por ID en SupplierController

--- src/main/java/com/northwind/controller/SupplierController.java
+++ src/main/java/com/northwind/controller/SupplierController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,6 +60,11 @@ public class SupplierController {
 		}else {
 			return ResponseEntity.notFound().build();
 		}
+	}
+	
+	@DeleteMapping("{supplierId}")
+	public void delete(@PathVariable int supplierId) {
+		supplierRepository.deleteById(supplierId);
 	}
 }
 
