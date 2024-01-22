fad34f1dd005e6386f19a18b76dffe6dd9bc1b7c
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 17:53:04 -0500

    Actualizar ShippersController con anotaciones y dependencia de ShippersRepository

--- src/main/java/com/northwind/controller/CustomerController.java
+++ src/main/java/com/northwind/controller/CustomerController.java
@@ -1,10 +1,10 @@
 package com.northwind.controller;
 
 import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/northwind/controller/ShippersController.java
+++ src/main/java/com/northwind/controller/ShippersController.java
@@ -1,5 +1,16 @@
 package com.northwind.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.northwind.repository.ShippersRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "shippers")
 public class ShippersController {
 
+	ShippersRepository shippersRepository;
 }
