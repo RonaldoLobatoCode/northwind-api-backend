a9ec2fa1c2bfa2344cfa0c98cf946550f8f686e2
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 11:35:05 -0500

    Agregar endpoint DELETE '/employees/{employeeId}' para eliminar un empleado por ID en EmployeeController

--- src/main/java/com/northwind/controller/EmployeeController.java
+++ src/main/java/com/northwind/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,6 +59,11 @@ public class EmployeeController {
 		}else {
 			return ResponseEntity.notFound().build();
 		}
+	}
+	
+	@DeleteMapping("{employeeId}")
+	public void delete(@PathVariable int employeeId) {
+		employeeRepository.deleteById(employeeId);
 	}
 }
 
