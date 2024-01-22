6764b18e1de0a0e2d5cc28cc3dfda058977f10bb
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 17:47:14 -0500

    Finalizado el desarrollo de la clase Shippers en el paquete entity

--- src/main/java/com/northwind/entity/Shippers.java
+++ src/main/java/com/northwind/entity/Shippers.java
@@ -1,5 +1,26 @@
 package com.northwind.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "Shippers")
+@Data
 public class Shippers {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "shipperid")
+	private int shipperId;
+	
+	@Column(name = "shippername")
+	private String shipperName;
+	
+	@Column(name = "phone")
+	private String phone;
 }
