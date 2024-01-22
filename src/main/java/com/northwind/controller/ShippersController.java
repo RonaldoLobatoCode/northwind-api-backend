d2fd5f849299a9e2550f7407cfa0e142558b081b
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 17:57:47 -0500

    Agregar endpoint GET '/shippers' en ShippersController para obtener todos los transportistas

--- src/main/java/com/northwind/controller/ShippersController.java
+++ src/main/java/com/northwind/controller/ShippersController.java
@@ -1,8 +1,12 @@
 package com.northwind.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.northwind.entity.Shippers;
 import com.northwind.repository.ShippersRepository;
 
 import lombok.AllArgsConstructor;
@@ -13,4 +17,9 @@ import lombok.AllArgsConstructor;
 public class ShippersController {
 
 	ShippersRepository shippersRepository;
+	
+	@GetMapping
+	public List<Shippers> findAll(){
+		return shippersRepository.findAll();
+	}
 }
