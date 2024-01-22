ad142257f7e6fbd7b7036eee2470e407b2c87ad4
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-22 18:38:37 -0500

    Finalizado el desarrollo de la clase Categorie en el paquete entity

--- src/main/java/com/northwind/entity/Categorie.java
+++ src/main/java/com/northwind/entity/Categorie.java
@@ -1,5 +1,26 @@
 package com.northwind.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "Categories")
+@Data
 public class Categorie {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "CategoryID")
+	private int categoryId;
+	
+	@Column(name = "categoryname")
+	private String categoryName;
+	
+	@Column(name = "description")
+	private String category;
 }
