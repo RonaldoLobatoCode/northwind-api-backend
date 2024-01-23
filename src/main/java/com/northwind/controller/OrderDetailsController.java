d1594c2d13fbfb86802c6cd354e76f9f725ac391
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-23 11:58:48 -0500

    Actualizar OrderDetailsController con anotaciones y dependencia de OrderDetailsRepository

--- src/main/java/com/northwind/controller/OrderDetailsController.java
+++ src/main/java/com/northwind/controller/OrderDetailsController.java
@@ -1,5 +1,16 @@
 package com.northwind.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.northwind.repository.OrderDetailsRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor 
+@RequestMapping(path = "order-details")
 public class OrderDetailsController {
 
+	OrderDetailsRepository orderDetailsRepository;
 }
