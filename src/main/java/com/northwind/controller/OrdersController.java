28ddbd6bdbbfbe71413f8d57b125ec60b661cb73
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-23 11:20:42 -0500

    Agregar endpoint POST '/orders' para registrar ordenes en OrdersController

--- src/main/java/com/northwind/controller/OrdersController.java
+++ src/main/java/com/northwind/controller/OrdersController.java
@@ -2,9 +2,12 @@ package com.northwind.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +31,11 @@ public class OrdersController {
 	@GetMapping("{orderId}")
 	public ResponseEntity<Orders> findById(@PathVariable int orderId){
 		return ResponseEntity.of(ordersRepository.findById(orderId));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Integer> register(@RequestBody Orders orders){
+		Orders save = ordersRepository.save(orders);
+		return ResponseEntity.status(HttpStatus.CREATED).body(save.getOrderId());
 	}
 }
