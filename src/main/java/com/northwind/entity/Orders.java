fb84a18594efd9ec75a5772fe526312e9b6b93a8
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-23 10:58:45 -0500

    Finalizado el desarrollo de la clase Orders en el paquete entity

--- src/main/java/com/northwind/entity/Orders.java
+++ src/main/java/com/northwind/entity/Orders.java
@@ -1,5 +1,34 @@
 package com.northwind.entity;
 
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "Orders")
+@Data
 public class Orders {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "orderid")
+	private int orderId;
+	
+	@Column(name = "customerid")
+	private int customerId;
+	
+	@Column(name = "employeeid")
+	private int employeeId;
+	
+	@Column(name = "orderdate")
+	private Date orderDate;
+	
+	@Column(name = "shipperid")
+	private int shipperId;
 }
