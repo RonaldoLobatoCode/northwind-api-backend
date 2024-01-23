de9401dd6b0baf8bc9e875c4a754cfe25be83a0c
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-23 11:27:58 -0500

    Agregar endpoint PUT '/orders/{orderId}' para actualizar una orden por ID en OrdersController

--- src/main/java/com/northwind/controller/OrdersController.java
+++ src/main/java/com/northwind/controller/OrdersController.java
@@ -1,12 +1,14 @@
 package com.northwind.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,4 +40,39 @@ public class OrdersController {
 		Orders save = ordersRepository.save(orders);
 		return ResponseEntity.status(HttpStatus.CREATED).body(save.getOrderId());
 	}
+	
+	@PutMapping("{orderId}")
+	public ResponseEntity<Orders> update(@PathVariable int orderId, @RequestBody Orders updatedOrder){
+		Optional<Orders> optionalOrders = ordersRepository.findById(orderId);
+		if(optionalOrders.isPresent()) {
+			Orders existingOrders = optionalOrders.get();
+			existingOrders.setCustomerId(updatedOrder.getCustomerId());
+			existingOrders.setEmployeeId(updatedOrder.getEmployeeId());
+			existingOrders.setOrderDate(updatedOrder.getOrderDate());
+			existingOrders.setShipperId(updatedOrder.getShipperId());
+			
+			Orders entity = ordersRepository.save(existingOrders);
+			return ResponseEntity.ok(entity);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
