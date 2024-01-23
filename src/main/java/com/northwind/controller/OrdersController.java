9331950c4a1b78b92776daf0179d4c10c0ed483f
RonaldoLobatoCode <codelobatodev99@gmail.com>
2024-01-23 11:08:21 -0500

    Agregar endpoint GET '/orders' en OrdersController para obtener todas las ordenes

--- src/main/java/com/northwind/controller/OrdersController.java
+++ src/main/java/com/northwind/controller/OrdersController.java
@@ -1,8 +1,12 @@
 package com.northwind.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.northwind.entity.Orders;
 import com.northwind.repository.OrdersRepository;
 
 import lombok.AllArgsConstructor;
@@ -13,4 +17,9 @@ import lombok.AllArgsConstructor;
 public class OrdersController {
 
 	OrdersRepository ordersRepository;
+	
+	@GetMapping
+	public List<Orders> findAll(){
+		return ordersRepository.findAll();
+	}
 }
